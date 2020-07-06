d2b30bb0e2dd244e7b3f9d25726ed78ee25784ef
Glitch (hello-webpage) <none>
2020-07-06 20:01:17 +0000

    💄👞 Checkpoint ./script.js:18584580/672 ./index.html:18584580/2473

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+// Name any p5.js functions we use in the global so Glitch can recognize them.
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+/* global createCanvas, background, ellipse */
+
+// Content behind double slashes is a comment. Use it for plain English notes,
+// or for code that you want to temporarily disable.
+
+function setup(){
+  // Code here runs only once
+  createCanvas(800, 600)
+}
+
+function draw(){
+  // Code here runs continuously
+  background(220)
+
+  ellipse(50, 50, 50, 50)
+
+}
