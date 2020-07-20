032f439273fcaa823320485f69cdbd3e8c3a613c
Glitch (day9-cssi-cohort13) <none>
2020-07-20 20:59:30 +0000

    ☔️👩‍❤️‍💋‍👩 Checkpoint ./bravia.js:18584580/3173 ./index.html:18584580/12

---
+++ warmer.js
@@ -0,0 +1,25 @@
+let backgroundColor, spherePosition, rectPosition
+
+function setup() {
+  // Canvas & color settings
+  createCanvas(500, 400);
+  colorMode(HSB, 360, 100, 100);
+  backgroundColor = 95;
+  // This variable contains a JSON object
+  spherePosition = {
+    "x": 100,
+    "y": 100
+  }
+}
+
+function draw() {
+  background(backgroundColor);
+  ellipse(spherePosition.x, spherePosition.y, 20, 20);
+  // rect(rectPosition.x, rectPosition.y, 20, 20);
+  // line(spherePosition.x, spherePosition.y, rectPosition.x, rectPosition.y);
+}
+
+function mousePressed() {
+  spherePosition.x = random(width);
+  spherePosition.y = random(height);
+}
