aa5c545319beca79db54783c4efc63f24cf38c2a
Glitch (day9-cssi-cohort13) <none>
2020-07-24 22:10:43 +0000

    🏁🍋 Checkpoint ./snake.js:18080260/4065 ./index.html:18080260/9 ./p5.gui.js:18080260/6932

---
+++ game.js
@@ -0,0 +1,21 @@
+/* global createCanvas, colorMode, HSB, background, ellipse, rect, text, 
+mouseX, mouseY, round, sqrt, backgroundColor, color, random, width, height
+frameRate, stroke, noFill, noStroke, keyCode,fill,collideRectRect, UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW, loop, noLoop
+*/
+
+// Since this example code uses the p5 collide2d library, be sure to remind
+// students to load it in. Model how to do this by either connecting a local
+// copy (included in the templates), connecting a hosted copy through a CDN, or
+// (as a last resort) by pasting it in its entirety in this script as the first
+// line.
+
+let backgroundColor, playerSnake, currentApple, score, rate, gameIsOver, lives;
+
+function setup() {
+  // Canvas & color settings
+}
+
+function draw() {
+  
+  }
+
