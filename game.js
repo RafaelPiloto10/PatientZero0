476ff202f1151ce3cb5895dd0a814467740cd335
Glitch (day9-cssi-cohort13) <none>
2020-07-24 22:48:35 +0000

    🎟🎇 Checkpoint ./game.js:18080260/766

--- game.js
+++ game.js
@@ -1,13 +1,5 @@
-/* global createCanvas, colorMode, HSB, background, ellipse, rect, text, 
-mouseX, mouseY, round, sqrt, backgroundColor, color, random, width, height
-frameRate, stroke, noFill, noStroke, keyCode,fill,collideRectRect, UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW, loop, noLoop
+/* global 
 */
-
-// Since this example code uses the p5 collide2d library, be sure to remind
-// students to load it in. Model how to do this by either connecting a local
-// copy (included in the templates), connecting a hosted copy through a CDN, or
-// (as a last resort) by pasting it in its entirety in this script as the first
-// line.
 
 
 function setup() {
@@ -16,5 +8,25 @@ function setup() {
 
 function draw() {
   
-  }
+}
 
+
+
+class Region{
+  constructor()
+  {
+    
+  }
+}
+
+class Citizen{
+  constructor(){
+    
+  }
+}
+
+class Map{
+  constructor(){
+    
+  }
+}
