af6d1b84c0566981e0ec521fc399a7cd5bdad274
Glitch (day9-cssi-cohort13) <none>
2020-07-25 18:11:46 +0000

    🚆🍮 Checkpoint ./state.js:18584546/1362 ./country.js:18584546/367

--- country.js
+++ country.js
@@ -1,5 +1,11 @@
 class Country{
-  constructor() {
+  constructor(num_states, init_funds, init_ppe) {
+    this.num_states = num_states;
+    this.funds = init_funds;
+    this.ppe = init_ppe;
     
+    for(let i = 0; i < this.num_states; i++){
+      
+    }
   }
 }
--- state.js
+++ state.js
@@ -1,3 +1,22 @@
+/* global createVector */
+
+/*
+  State class meant to simulate an individual state with unique state properties
+*/
 class State {
-  constructor() {}
+  /*
+    Constructor for the State class
+    @param lon (float) - The longitude of the state
+    @param lat (float) - The latitude of the state
+    @param id (string) - the id abbreviate of the state ie. CA
+    @param pop (int) - the population of the state
+    @param pop_density (float) - the population density of the state
+  
+  */
+  constructor(lon, lat, id, pop, pop_density) {
+    this.coords = createVector(lon, lat);
+    this.id = id;
+    this.population = pop;
+    this.population_density = pop_density;
+  }
 }
