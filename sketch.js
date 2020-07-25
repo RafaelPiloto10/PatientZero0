8f6514ab269a6c55c7d4ab768ce840a4b85cdbbc
Glitch (day9-cssi-cohort13) <none>
2020-07-25 21:23:29 +0000

    😼👩 Checkpoint ./tools.js:18584546/15020 ./sketch.js:18584546/7 ./index.html:18584546/260

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ let game;
 let tot_num_of_states = 50;
 
 function setup() {
-  game = new Game("2019PopulationEstimate.csv", tot_num_of_states);
+  // game = new Game("2019PopulationEstimate.csv", tot_num_of_states);
 }
 
 function draw() {}
--- tools.js
+++ tools.js
@@ -5,3 +5,30 @@ function file_to_csv(file_name) {
       .then(response => resolve($.csv.toObjects(response)));
   });
 }
+
+function read_json(file_name) {
+  return new Promise( async (resolve, reject) => {
+    await fetch(file_name)
+      .then(response => response.json())
+      .then(response => resolve(response));
+  });
+}
+
+let complete_states = [];
+
+read_json("data.json").then((states) => {
+  for(let i = 0; i < states.length; i++){
+    for(let j = 0; i < state_coords.length; j++){
+      if(states[i].State == state_coords[i].state) {
+        complete_states.push({
+          ...state_coords[i],
+          states[i].Pop,
+          states[i].Density,
+          states[i].LandArea
+        });
+      }
+    }
+  }
+  console.log()
+});
+
