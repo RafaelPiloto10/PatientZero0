5ac3ff775cede40fd4ff250781f34985c22f6887
Glitch (day9-cssi-cohort13) <none>
2020-07-25 18:23:28 +0000

    📹💴 Checkpoint ./country.js:18584546/4991 ./state.js:18584546/2020

--- country.js
+++ country.js
@@ -1,11 +1,60 @@
-class Country{
-  constructor(num_states, init_funds, init_ppe) {
+/* global random */
+
+/*
+  The country class meant to represent the entire country the player is trying to save
+*/
+class Country {
+  /*
+    Constructor for the country class
+    
+    @param num_states (int) - the total number of states in the country
+    @param init_funds (float) - the starting amount of funds for the country
+    @param init_ppe (int) - the starting amount of ppe for the country
+    @param init_spread_rate (float) - the initial spread rate of the virus
+    @param init_infected (int) - the starting amount of people infected at the beginning of the game
+    
+  */
+  constructor(
+    num_states,
+    init_funds,
+    init_ppe,
+    init_spread_rate,
+    init_infected
+  ) {
     this.num_states = num_states;
+    this.states = [];
     this.funds = init_funds;
     this.ppe = init_ppe;
+
+    this.deaths = 0;
+    this.total_infected = init_infected;
+    this.init_infected = init_infected;
+    this.recovered = 0;
+    this.spread_rate = init_spread_rate;
+
+    this.populate_states();
+    this.patient_zero();
+  }
+
+  /*
+    Populate the states with random/seeded configurations
     
-    for(let i = 0; i < this.num_states; i++){
-      
+    @return None
+  */
+  populate_states() {
+    for (let i = 0; i < this.num_states; i++) {
+      // ---------------------- TODO: Need to create JSON file with state information ---------------------------------------
+      // this.states.push(new State(lat, lon, id, population, pop_density));
+    }
+  }
+  
+  /*
+    Creates patient zer0s based on the intial amount of citizens infected
+  */
+  patient_zero() {
+    for (let i = 0; i < this.init_infected; i++) {
+      let random_state = random(this.states);
+      random_state.infect();
     }
   }
 }
--- state.js
+++ state.js
@@ -11,12 +11,19 @@ class State {
     @param id (string) - the id abbreviate of the state ie. CA
     @param pop (int) - the population of the state
     @param pop_density (float) - the population density of the state
-  
+    @param revenue (float) - The amount of money the state contributes in revenue towards the country
   */
-  constructor(lon, lat, id, pop, pop_density) {
+  constructor(lon, lat, id, pop, pop_density, state_init_infected, revenue) {
     this.coords = createVector(lon, lat);
     this.id = id;
     this.population = pop;
     this.population_density = pop_density;
+
+    this.state_deaths = 0;
+    this.state_infected = state_init_infected;
+    this.state_recovered = 0;
+    
+    this.revenue = revenue
+    this.state_ppe = 
   }
 }
