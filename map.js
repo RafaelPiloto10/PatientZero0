ecd1048d019328b313f4b4cff684321ae381bdb5
Glitch (day9-cssi-cohort13) <none>
2020-07-27 23:46:41 +0000

    🌶🍕 Checkpoint ./map.js:18080260/69

--- map.js
+++ map.js
@@ -18,12 +18,12 @@ class Map {
   
   drawCases()
   {
-    //noStroke();
+    noStroke();
     for(let i=0;i<simulation.country.states.length;i++) //pulls the data of each state to draw a circle on its "current" location
     {
       let pos = myMap.latLngToPixel(simulation.country.states[i].coords.x,simulation.country.states[i].coords.y);
-      let size = state_data[i].LandArea/500;
-      fill(0, 100.0*simulation.country.states[i].state_infected/simulation.country.states[i].population,100, .5);
+      let size = state_data[i].LandArea*Math.pow(myMap.zoom(),3)/200000;
+      fill(0,200.0*simulation.country.states[i].state_infected/simulation.country.states[i].population,100);
       circle(pos.x,pos.y,size);
     }
   }
