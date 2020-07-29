629d32cf7d9c9b954b8156e15b11ef046fb13c7c
Glitch (day9-cssi-cohort13) <none>
2020-07-29 19:02:21 +0000

    ⛑⛪️ Checkpoint ./index.html:18584546/385 ./sketch.js:18584546/331

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ let simulation;
 let tot_num_of_states = 51;
 let canvas;
 let mmap;
+let graph;
 
 function setup() {
   colorMode(HSB, 360, 100, 100);
@@ -11,6 +12,12 @@ function setup() {
   simulation = new Simulation(tot_num_of_states, "21 January 2020");
   mmap = new Map();
   createButtons();
+  graph = new CanvasJS.Chart("chartContainer", {
+    e
+    title: { text: "New Cases per Day" },
+    axisY: { title: "New Confirmed Cases" },
+    data: [{type: "splineArea", color:"rgba(255, 0, 0, .7)", markerSize: 5}]
+  })
 }
 
 function draw() {
